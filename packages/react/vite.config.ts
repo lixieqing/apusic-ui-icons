cc7cb1ef85300005044fcf07af744378168fcd6b
lixieqing <10072414+lixieqing@users.noreply.github.com>
2024-11-22 17:56:48 +0800

    update: 依赖更新

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { terser } from "rollup-plugin-terser";
-import dts from 'vite-plugin-dts';
+import dts from "vite-plugin-dts";
 
 // 基础配置：设置构建环境和输出格式
 export default defineConfig({
@@ -28,7 +28,7 @@ export default defineConfig({
     react(),
     dts({
       insertTypesEntry: true, // 在 package.json 中自动声明 types 字段
-      outDir: "dist/types", // 声明文件的输出目录
+      outDir: "dist", // 声明文件的输出目录
     }),
     terser({
       compress: {
